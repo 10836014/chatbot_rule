e6535c6b54fbdd335f56464ba1bea5244b17de5b
10836014 <10836014@ntub.edu.tw>
2020-06-03 20:16:12 +0800

    把可以跑flask和跑localhost的檔案分開

--- 可以跑flask.py/app.py
+++ 可以跑flask.py/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import Chatbot.chatbot as Chatbot
+
 import json
 
 app = Flask(__name__)
@@ -7,7 +8,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-
     if request.method == 'POST':
         data = json.loads(request.get_data().decode('utf-8'))
         print("get POST request data: ", data)
@@ -25,5 +25,6 @@ def hello_world():
     elif request.method == 'GET':
         return 'GET OK!'
 
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
